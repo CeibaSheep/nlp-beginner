a2fbeef71237dc6b1655e92409bc310031a54433
CeibaSheep <liyangchq@163.com>
2019-06-28 14:47:35 +0800

    save

--- bag-of-word.py
+++ bag-of-word.py
@@ -2,17 +2,53 @@
 
 import numpy as np
 import pandas as pd
+import re
 
+
+def preprocess(text):
+    text = text.lower()
+    text = text.replace('.', '')
+    text = text.replace('!?', '')
+    text = text.replace('"', '')
+    text = text.replace('#', '')
+    text = text.replace('!', '')
+    text = text.replace('$', '')
+    text = text.replace('\'', '')
+    text = text.replace('&', '')
+    text = text.replace('--', '')
+    text = text.replace('+', '')
+    text = text.replace(',','')
+    text = text.replace('-','')
+    text = text.replace(':','')
+    text = text.replace(';','')
+    text = text.replace('=','')
+    text = text.replace('?','')
+    text = text.replace('\*','')
+    text = text.replace('`','')
+    text = text.replace('\/', '')
+    text = re.sub('[0-9]+[a-zA-Z]', '', text)
+    text = re.sub('\d', '', text)
+    words = text.split()
+    return words
+        
 #def read_data():
 td = pd.read_csv('./dataset/train.tsv', header=0, delimiter='\t')
 x_train = td['Phrase']
 y_train = td['Sentiment']
 words = []
-vocabs = []
 for item in x_train:
-    words += item.split(' ')
-#print('done')
-#vocabs = set(words)
-#x_dim = len(vocabs)
-#vocab_to_int = {word: i for i, word in vocabs}
-#int_to_vocab = {i: word for i, word in vocabs}
+    words += preprocess(item)
+vocabs = set(words)
+vocab_to_int = {word: i for i, word in enumerate(vocabs)}
+row_dim = len(x_train)
+col_dim = len(vocabs)
+
+
+vocab_matrix = []
+for i in range(row_dim):
+    word_list = list(set(x_train[i].split()))
+    tmp = np.zeros(col_dim)
+    for j, word in enumerate(vocabs):
+        tmp[j] = x_train[i].count(word)
+    vocab_matrix.append(tmp)
+
